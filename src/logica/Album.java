a407cba0b6768c982fa0b8f4fd5813a348629a7f
Juli Nuñez <njulianacamila14@gmail.com>
2022-10-22 22:20:36 -0300

    add constructor de album

--- src/logica/Album.java
+++ src/logica/Album.java
@@ -4,6 +4,16 @@ public class Album {
 	
 	private boolean[] figuritas; 
 	private boolean[] repetidas;
+	private int cantFiguritas;
+	
+	
+	public Album(int cantFiguritas) {;
+		this.figuritas = new boolean[cantFiguritas];
+		this.repetidas = new boolean[cantFiguritas];
+		this.cantFiguritas = cantFiguritas;
+	}
+	
+	
 	
 	
 	
