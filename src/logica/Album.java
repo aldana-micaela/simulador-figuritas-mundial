52af574f960aff860ddde1e377439e161070d327
Juli Nuñez <njulianacamila14@gmail.com>
2022-10-22 22:26:13 -0300

    add metodo album

--- src/logica/Album.java
+++ src/logica/Album.java
@@ -3,18 +3,25 @@ package logica;
 public class Album {
 	
 	private boolean[] figuritas; 
-	private boolean[] repetidas;
+	private int[] repetidas;
 	private int cantFiguritas;
 	
 	
 	public Album(int cantFiguritas) {;
 		this.figuritas = new boolean[cantFiguritas];
-		this.repetidas = new boolean[cantFiguritas];
+		this.repetidas = new int[cantFiguritas];
 		this.cantFiguritas = cantFiguritas;
 	}
 	
 	
-	
+	public void agregarFiguritas(int numerofigurita) {
+		if(figuritas[numerofigurita]==true) {
+			repetidas[numerofigurita]=repetidas[numerofigurita]+1;
+		}else {
+			figuritas[numerofigurita]=true;
+
+		}
+	}
 	
 	
 
