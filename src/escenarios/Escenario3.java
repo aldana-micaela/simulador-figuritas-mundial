eca8900d2227e12357b5ca0a76d2ef2ce6933ac1
Juli Nuñez <njulianacamila14@gmail.com>
2022-11-01 00:05:20 -0300

    add escenario3

--- src/escenarios/Escenario3.java
+++ src/escenarios/Escenario3.java
@@ -1,53 +1,102 @@
 package escenarios;
 
-
 import java.util.ArrayList;
 
 import codigoNegocio.Usuario;
 import generador.Generador;
 
 public class Escenario3 implements Escenario {
-	
+
 	private int cantUsuarios;
-	private int donadas;
+	private int intercambiadas;
+	ArrayList<Usuario> lista;
 
-	public Escenario3 (int cantUsuario) {
+	public Escenario3(int cantUsuario) {
 		this.cantUsuarios = cantUsuario;
-		this.donadas=0;
-		
-	}
+		this.intercambiadas = 0;
 
+	}
 
 	@Override
 	public void generarPaquete(Usuario u, Generador g) {
-		// TODO Auto-generated method stub
-		
+		int i = 0;
+		while (i < u.getCantFiguritasXPaquete()) {
+
+			u.agregarFigurita(g.nextInt(u.getCantFiguritas()));
+			IntercambiarRepetidas(u);
+
+			i++;
+		}
+
 	}
-
-
 
 	@Override
 	public int getCantUsuarios() {
 		return cantUsuarios;
 	}
 
-
 	@Override
 	public void simular(Usuario u, Generador g, ArrayList<Usuario> lista) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public int getDonadas() {
-		
-		return donadas;
+		this.lista = lista;
+		generarPaquete(u, g);
+
 	}
 
+	private void IntercambiarRepetidas(Usuario user) {
+		int f=0;
+		for (Usuario u : lista) {
+
+			if (user.getNumeroUsuario() != u.getNumeroUsuario()) {
+
+				for (int i = 0; i < u.getCantFiguritas(); i++) {
+
+					if (user.getFiguritasRepetidas()[i] > 0 && u.getAlbumFiguritas()[i]) {
+						f=intercambioDefiguritas(user,u);
+						if(f>-1) {
+							u.getAlbumFiguritas()[i] = true;
+							u.incrementarFiguritasAcertadas();
+							user.decrementarFiguritasRepetidas(i);
+							intercambiadas++;
+							
+							user.getAlbumFiguritas()[f] =true;
+							user.incrementarFiguritasAcertadas();
+							u.decrementarFiguritasRepetidas(f);
+							intercambiadas++;
+						}
+						
+					}
+				}
+			}
+
+		}
+
+	}
+
+	private int intercambioDefiguritas(Usuario user, Usuario u) {
+		for (int i = 0; i < u.getCantFiguritas(); i++) {
+			if(u.getFiguritasRepetidas()[i] > 0 && user.getAlbumFiguritas()[i]) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public int getDonadas() {
+
+		return intercambiadas;
+	}
 
 	@Override
 	public int getNoDonadas() {
-		// TODO Auto-generated method stub
-		return 0;
+		int i = 0;
+
+		for (Usuario u : lista) {
+			for (int j = 0; j < u.getCantFiguritas(); j++) {
+
+				i = i + u.getFiguritasRepetidas()[j];
+			}
+		}
+		return i;
 	}
 
 }
